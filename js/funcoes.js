44d8e909e7a2d2ed825311864ec42a98733cb649
Joao Pedro <jpremedi@hotmail.com>
2020-12-14 20:44:00 -0300

    projeto calculadora

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,61 +1,5 @@
-function Inserir0() {
-    document.getElementById("visor").value += 0;
-}
-
-function Inserir7() {
-    document.getElementById("visor").value += 7;
-}
-
-function Inserir8() {
-    document.getElementById("visor").value += 8;
-}
-
-function Inserir9() {
-    document.getElementById("visor").value += 9;
-}
-
-function Inserir4() {
-    document.getElementById("visor").value += 4;
-}
-
-function Inserir5() {
-    document.getElementById("visor").value += 5;
-}
-
-function Inserir6() {
-    document.getElementById("visor").value += 6;
-}
-
-function Inserir1() {
-    document.getElementById("visor").value += 1;
-}
-
-function Inserir2() {
-    document.getElementById("visor").value += 2;
-}
-
-function Inserir3() {
-    document.getElementById("visor").value += 3;
-}
-
-function InserirMais() {
-    document.getElementById("visor").value += "+";
-}
-
-function InserirVezes() {
-    document.getElementById("visor").value += "*";
-}
-
-function InserirDividir() {
-    document.getElementById("visor").value += "/";
-}
-
-function InserirMenos() {
-    document.getElementById("visor").value += "-";
-}
-
-function InserirPonto() {
-    document.getElementById("visor").value += ".";
+function Inserir(elemento) {
+    document.getElementById("visor").value += elemento;
 }
 
 function LimparVisor() {
@@ -78,7 +22,7 @@ function Calcular() {
     document.getElementById("visor").value = resultado;
 }
 
-function Apagar() { 
+function Apagar() {
     let conteudo = document.getElementById("visor").value;
     document.getElementById("visor").value = conteudo.substring(0, conteudo.length - 1);
 }
